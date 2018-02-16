b3eb38027e3d39ba925e31898be0c996f4ece856
NEHA AGARWAL <500061675@DDN.UPES.AC.IN>
2018-02-16 22:43:37 +0530

    sign up form

---
+++ SignUp/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SIGN UP</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style>
+    /*body {
+        background-repeat: no-repeat;
+        background-size: 1280px 700px;
+        -webkit-appearance: none;
+    }
+
+    .error {
+        color: #FF0000;
+    }
+
+    .form {
+        width: 400px;
+        height: 35px;
+        border-radius: 20px;
+        border: 2px solid #dcdbdb;
+        padding: 20px;
+
+    }
+
+    textarea:focus,
+    input:focus {
+        outline: none;
+    }
+
+    ::-webkit-input-placeholder {
+        /* Chrome/Opera/Safari
+        opacity: 0.7;
+    }
+
+    .submit_center {
+        margin-top: 50px;
+        margin-left: 170px;
+    }*/
+
+</style>
+
+<body>
+    <?php
+// define variables and set to empty values
+$nameErr = $name1Err ="";
+$Fname = $Lname ="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if (empty($_POST["Fname"]))
+            {
+                $nameErr = "Name is required";
+            }
+        else
+            {
+                $name = test_input($_POST["Fname"]);
+                // check if name only contains letters and whitespace
+                if (!preg_match("/^[a-zA-Z ]*$/",$name))
+                {
+                $nameErr = "Only letters and white space allowed";
+                }
+            }
+     }
+  if ($_SERVER["REQUEST_METHOD"] == "POST")
+       {
+           if (empty($_POST["Lname"]))
+               {
+                    $name1Err = "Name is required";
+                }
+            else
+              {
+                    $name1 = test_input($_POST["Lname"]);
+                    // check if name only contains letters and whitespace
+                    if (!preg_match("/^[a-zA-Z ]*$/",$name1))
+                    {
+                    $name1Err = "Only letters and white space allowed";
+                    }
+             }
+  }
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>*/
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-8">
+                    <div style="margin-top:150px">
+                        <p><span class="error">* required field.</span></p>
+                        <form method="post" action="insert_signup.php">
+                            <div class="form-group">
+                                <label for="f_name"></label>
+                                <input type="text" class="form" name="f_name" placeholder="First Name"><span class="error">* <?php echo $nameErr;?></span>
+                            </div><br>
+                            <div class="form-group">
+                                <label for="l_name"></label>
+                                <input type="text" class="form" name="l_name" placeholder="Last Name"><span class="error">* <?php echo $name1Err;?></span>
+                            </div><br>
+                            <div class="form-group">
+                                <label for="phone_no"></label>
+                                <input type="text" class="form" name="phone_no" placeholder="Phone No">
+                            </div><br>
+                            <br>
+                            <div class="submit_center">
+                                <button type="sub" class="btn btn-primary" name="sub">Submit</button>
+                            </div>
+                        </form>
+                        <div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</body>
+</html>
---
+++ SignUp/insert_signup.php
@@ -0,0 +1,28 @@
+ <?php
+$con=mysqli_connect("localhost","root","","agriculture");
+    echo "hello";
+if(isset($_POST['sub']))
+{
+    echo "HELLO";
+   $a=$_POST['f_name'];
+   $b=$_POST['l_name'];
+   $g=$_POST['phone_no'];
+            $query=mysqli_query($con,"INSERT INTO user (f_name,l_name,phone_no)VALUES('$a','$b','$g')");
+                 if($query)
+                         {
+                     echo "hello";
+							 /* $query2=mysqli_query($con,"SELECT* FROM user WHERE phone_no='$g'");
+							  while($row=mysqli_fetch_array($query2))
+                                 {
+                                  $k                                                                                                          k=$row['email'];
+									 $n=$row['user_id'];
+									  session_id("session2");
+									 session_start();
+                                    $_SESSION['login_email']=$kk;
+							        $_SESSION['id']=$n;
+                                    header("Location:select_user_type.php");
+                                 }*/
+						 }
+
+   }
+?>
