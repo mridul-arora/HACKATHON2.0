4b773b2925a86abe36798c53365cbdf3f5bafc00
500060750 <mayuri.kishore123@gmail.com>
2018-02-16 23:49:13 +0530

    a

---
+++ approximate price/index.php
@@ -0,0 +1,92 @@
+<html>
+<head>
+</head>
+<body>
+<style>
+<div class="speakers-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-xs-10">
+					<div class="section-title">
+						<p>Meet our specialist</p>
+						<h2>Our speakers</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="speaker-slider-hm2">
+						<figure class="single-speaker">
+							<img src="img/hm-2-speaker/01.png" alt="img">
+							<figcaption class="single-speaker-content">
+								<div class="cap-border"></div>
+								<div class="cap-border-2"></div>
+								<a href="#">
+									<h3>dabid jhonshon</h3>
+								</a>
+								<p>ceo & founder</p>
+							</figcaption>
+						</figure>
+						<figure class="single-speaker">
+							<img src="img/hm-2-speaker/02.png" alt="img">
+							<figcaption class="single-speaker-content">
+								<div class="cap-border"></div>
+								<div class="cap-border-2"></div>
+								<a href="#">
+									<h3>dabid jhonshon</h3>
+								</a>
+								<p>ceo & founder</p>
+							</figcaption>
+						</figure>
+						<figure class="single-speaker">
+							<img src="img/hm-2-speaker/03.png" alt="img">
+							<figcaption class="single-speaker-content">
+								<div class="cap-border"></div>
+								<div class="cap-border-2"></div>
+								<a href="#">
+									<h3>Thomas Alva</h3>
+								</a>
+								<p>ceo & founder</p>
+							</figcaption>
+						</figure>
+						<figure class="single-speaker">
+							<img src="img/hm-2-speaker/03.png" alt="img">
+							<figcaption class="single-speaker-content">
+								<div class="cap-border"></div>
+								<div class="cap-border-2"></div>
+								<a href="#">
+									<h3>dabid jhonshon</h3>
+								</a>
+								<p>ceo & founder</p>
+							</figcaption>
+						</figure>
+						<figure class="single-speaker">
+							<img src="img/hm-2-speaker/02.png" alt="img">
+							<figcaption class="single-speaker-content">
+								<div class="cap-border"></div>
+								<div class="cap-border-2"></div>
+								<a href="#">
+									<h3>Thomas Alva</h3>
+								</a>
+								<p>ceo & founder</p>
+							</figcaption>
+						</figure>
+						<figure class="single-speaker">
+							<img src="img/hm-2-speaker/03.png" alt="img">
+							<figcaption class="single-speaker-content">
+								<div class="cap-border"></div>
+								<div class="cap-border-2"></div>
+								<a href="#">
+									<h3>dabid jhonshon</h3>
+								</a>
+								<p>ceo & founder</p>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</style>
+</body>
+</html>
