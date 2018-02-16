5b03a801200090fbefbfada9a685045c54b9de74
mridul-arora <aroramridul18@gmail.com>
2018-02-17 01:38:49 +0530

    approximate alignment

--- approximate/index.php
+++ approximate/index.php
@@ -9,58 +9,54 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
+    <style>
+        .card{
+            margin: 2vh 3vw;
+
+        }
+    </style>
 <body>
- <div class="container">
-  <div class="row">
-  <h2></h2>
-  </div>
- <div class="row">
+   <div class="container">
+     <div class="row">
    <div class="col-md-6 col-xl-6 col-xs-12">
   <div class="card">
       <img src="../Assets/images/r.jpg">
-    <div class="card-body">RICE.</div>
   </div>
   </div>
   <div class="col-md-6 col-xl-6 col-xs-12">
   <div class="card">
        <img src="../Assets/images/w.jpg">
-    <div class="card-body">WHEAT</div>
   </div>
   </div>
+     </div>
+    </div>
    <div class="container">
-  <div class="row">
-  <h2></h2>
-  </div>
  <div class="row">
    <div class="col-md-6 col-xl-6 col-xs-12">
   <div class="card">
       <img src="../Assets/images/p.jpg">
-    <div class="card-body">POTATO</div>
   </div>
   </div>
   <div class="col-md-6 col-xl-6 col-xs-12">
   <div class="card">
       <img src="../Assets/images/b.jpg">
-    <div class="card-body">BLACKGRAM</div>
   </div>
   </div>
-
+       </div>
+     </div>
    <div class="container">
-  <div class="row">
-  <h2></h2>
-  </div>
  <div class="row">
    <div class="col-md-6 col-xl-6 col-xs-12">
   <div class="card">
       <img src="../Assets/images/c.jpg">
-    <div class="card-body">CABBAGE</div>
   </div>
   </div>
   <div class="col-md-6 col-xl-6 col-xs-12">
   <div class="card">
       <img src="../Assets/images/br.jpg">
-    <div class="card-body">BRINJAL</div>
   </div>
-  </div>
+     </div>
+       </div>
+    </div>
 </body>
 </html>
