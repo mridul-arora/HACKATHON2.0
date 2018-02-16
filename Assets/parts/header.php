dbba0a8936d206822a4a81cbd2dae304ca2eb3d0
mridul-arora <aroramridul18@gmail.com>
2018-02-17 00:15:05 +0530

    navbar

---
+++ Assets/parts/header.php
@@ -0,0 +1,46 @@
+<!--<header id="header">
+    <a href="#"><img src="" style="" /><span id="left"><strong>Rural Development</strong></span></a>
+    <div class="right">
+        <ul>
+            <li>
+                <class="navsize"><a href="AboutUs/index.php">About Us</a></class>
+            </li>
+            <li>
+                <class="navsize"><a href="SignIn/index.php">Sign In</a></class>
+            </li>
+            <li>
+                <class="navsize"><a href=" ContactUs/index.php ">Contact Us</a></class>
+            </li>
+            <li>
+                <class="navsize "><a href="SignUp/index.php ">Sign Up</a></class>
+            </li>
+            <li>
+                <class="navsize"><a href="SignOut/index.php ">Sign Out</a></class>
+            </li>
+        </ul>
+    </div>
+</header>-->
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Rural Development</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <!-- <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>-->
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="AboutUs/index.php">About Us</a></li>
+            <li><a href=" ContactUs/index.php ">Contact Us</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+    </div>
+</nav>
