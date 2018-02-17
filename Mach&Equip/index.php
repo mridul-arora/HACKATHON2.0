17b7cfb0f7c0f167c04d279147ed98b0c40e1cbc
NEHA AGARWAL <500061675@DDN.UPES.AC.IN>
2018-02-17 20:21:50 +0530

    neha

--- Mach&Equip/index.php
+++ Mach&Equip/index.php
@@ -9,40 +9,52 @@
 	margin-top:10em;
 	padding:45px;
    	text-align:center;
+    text-decoration: none;
 	color:white;
+    font-weight: 1000;
 	margin-left:2em;
 	margin-right:2em;
 	border-radius: 25px;
 
 }
+        a:hover{
+            color: white;
+            text-decoration: none;
+        }
         body{
-            background-image: url("../Assets/images/grass.jpg");
+            background-image: url("../Assets/images/urb.jpg");
            background-repeat: no-repeat;
             background-position: center;
             background-size: cover;
-        }
-        .row a
-        {
-            text-decoration: none;
         }
 
     </style>
     <body>
 <div class= "container">
+    <div class="row">
+        <div class="col-md-12"></div>
+        <CENTER><h1></h1></CENTER>
+    </div>
 	     <div class="row">
             <div class="col-md-6">
-				  <a href="#">
+				  <a href="available/index.php">
 				  <div class="select_platform_space " style="font-size:20px;">
-                  Buy Crops and Vegetables.
+                      <bold>AT SECOND HAND RATE</bold>
                   </div></a>
 			</div>
                <div class="col-md-6">
-      <a href="wheat_market_price.php">
-
+		          <a href="nee/index.php">
                   <div class="select_platform_space " style="font-size:20px;">
-                  Sell crops and vegetables and current market rate
+
+                      <bold>FOR RENTING</bold>
                   </div></a>
             </div>
+               <!--<div class="col-md-4">
+				  <a href="#">
+				  <div class="select_platform_space " style="font-size:20px;">
+                Sell Crops and Vegetables.
+                  </div></a>
+			</div>-->
 	     </div>
       </div>
 </body>
