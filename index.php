3441b79d23bd8cb885b8440b3d06120d3143e70d
500060750 <mayuri.kishore123@gmail.com>
2018-02-17 13:03:20 +0530

    IN

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <link rel="stylesheet" type="text/css" href="Assets/css/style.css" />
     <title>Agriculture</title>
     <meta charset="utf-8">
@@ -192,6 +193,103 @@
             </div>
         </div>
     </div>-->
+    </body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="Assets/css/style.css" />
+    <title>Agriculture</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>>
+</head>
+<style>
+    .box {
+        /*  margin: 2vh 3vw;*/
+        width: 250px;
+        height: 250px;
+    }
+
+    .left {
+        float: left;
+    }
+
+    .right {
+        float: right;
+        text-align: right;
+    }
+
+    .container {
+        margin: 2vh 3vw;
+    }
+    .img-fluid{
+        border: 0;
+    }
+    .card{
+        margin: 2vh 2vw;
+    }
+    .card-title{
+      text-align: center;
+        vertical-align: middle;
+        color: black;
+    }
+</style>
+<body>
+    <!--<div class="container">
+        <div class="row">
+            <div class="card img-fluid" style="width:500px">
+                <img class="card-img-top" src="../Assets/images/r.jpg" alt="Card image" <style="width:100%">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">RICE</h4>
+                </div>
+            </div>
+            <div class="card img-fluid" style="width:500px">
+                <img class="card-img-top" src="../Assets/images/w.jpg" alt="Card image" style="width:111%">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">WHEAT</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="card img-fluid" style="width:500px">
+                <img class="card-img-top" src="../Assets/images/p.jpg" alt="Card image" style="width:107%">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">POTATO</h4>
+                </div>
+            </div>
+            <div class="card img-fluid" style="width:500px">
+                <img class="card-img-top" src="../Assets/images/b.jpg" alt="Card image" style="width:110%">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">BLACK GRAM</h4>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="card img-fluid" style="width:500px">
+                <img class="card-img-top" src="../Assets/images/c.jpg" alt="Card image" style="width:130%">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">CABBAGE</h4>
+                </div>
+            </div>
+
+        <div class="card img-fluid" style="width:510px">
+            <img class="card-img-top" src="../Assets/images/br.jpg" alt="Card image" style="width:100% height:80%">
+            <div class="card-img-overlay">
+                <h4 class="card-title">BRINJAL</h4>
+            </div>
+        </div>
+    </div>
+    </div>-->
     <div class="container">
             <div class="card-deck">
                 <div class="card text-center">
@@ -263,5 +361,9 @@
         </div>
     </div>
 </div>
-    </body>
+</body>
+
 </html>
+
+
+
