a40a1569fbda6366a8663ff02cee86ae45d113bf
500060750 <mayuri.kishore123@gmail.com>
2018-02-17 12:20:01 +0530

    mk

--- Weather/index.php
+++ Weather/index.php
@@ -2,8 +2,126 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    <title>
-        Weather
-        </title>
+    <link rel="stylesheet" type="text/css" href="Assets/css/style.css" />
+    <title>WEATHER</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+    <Style>
+    .navbar.navbar-default {
+        margin-bottom: 0;
+    }
+     body {
+        background: #eee
+    }
 
-    </head>
+    .container-fluid {
+       /* max-width: 1200px;*/
+    }
+
+    header {
+        background: #563d7c;
+        padding: 2em 1em;
+        color: #fff;
+        margin-bottom: 2em;
+        h1 {
+            font-weight: 200;
+        }
+    }
+
+    .card {
+        background: #fff;
+        box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+        img {
+            width: 100%;
+        }
+    }
+
+    [class*="row"] {
+        margin-bottom: 1em;
+        justify-content: center;
+    }
+
+    [class*="col-"] {
+        background: #efefef;
+        border: 1px solid #ddd;
+        padding-top: .75rem;
+        padding-bottom: .75rem;
+        width: 100%;
+    }
+
+</Style>
+<body>
+    <!-- <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="AboutUs/index.php">About Us</a></li>
+             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href=" ContactUs/index.php ">Contact Us</a></li>
+        </ul>
+    </div>
+</nav>-->
+    <div class="col-sm-12">
+    <div class="row">
+      <div class="col-sm-7 five-three">
+        <div class="row">
+          <div class="col-sm-4">
+          <div class="card"><img alt="Card image cap" class="card-img-top img-fluid" src="img/brownie.jpg" />
+      <div class="card-block">
+        <h4 class="card-title">Brownie Delight</h4>
+
+        <p class="card-text">Our customer favorite chocolate ice cream jam packed with pieces of brownies and fudge</p>
+      </div>
+    </div>
+          </div>
+          <div class="col-sm-4">
+          <div class="card"><img alt="Card image cap" class="card-img-top img-fluid" src="img/butterPecan.jpg" />
+      <div class="card-block">
+        <h4 class="card-title">Butter Pecan</h4>
+
+        <p class="card-text">Roasted pecans, butter and vanilla come together to make this wonderful ice cream</p>
+      </div>
+    </div>
+          </div>
+          <div class="col-sm-4">
+          <div class="card"><img alt="Card image cap" class="card-img-top img-fluid" src="img/butterPecan.jpg" />
+      <div class="card-block">
+        <h4 class="card-title">Butter Pecan</h4>
+
+        <p class="card-text">Roasted pecans, butter and vanilla come together to make this wonderful ice cream</p>
+      </div>
+    </div>
+          </div><!-- end inner row -->
+        </div>
+      </div>
+      <div class="col-sm-5 five-two">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card"><img alt="Card image cap" class="card-img-top img-fluid" src="img/butterPecan.jpg" />
+      <div class="card-block">
+        <h4 class="card-title">Butter Pecan</h4>
+
+        <p class="card-text">Roasted pecans, butter and vanilla come together to make this wonderful ice cream</p>
+      </div>
+    </div>
+          </div>
+          <div class="col-sm-6">
+<div class="card"><img alt="Card image cap" class="card-img-top img-fluid" src="img/butterPecan.jpg" />
+      <div class="card-block">
+        <h4 class="card-title">Butter Pecan</h4>
+
+        <p class="card-text">Roasted pecans, butter and vanilla come together to make this wonderful ice cream</p>
+      </div>
+    </div>          </div>
+        </div><!-- end inner row -->
+      </div>
+    </div>​<!-- end outer row -->
+ </div>
+
+
+    </body>
