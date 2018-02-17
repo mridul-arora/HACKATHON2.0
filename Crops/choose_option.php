8e79c1bc2cbc27ee8a076129ef3fd757ca0c22c5
mridul-arora <aroramridul18@gmail.com>
2018-02-17 07:58:16 +0530

    choose_option.php

---
+++ Crops/choose_option.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+    <?php include "../Assets/parts/header.php";?>
+    </head>
+    <style>
+
+      .select_platform_space {
+	border:2px solid black;
+	margin-top:10em;
+	padding:45px;
+   background-color:rgb(153, 179, 255);
+	text-align:center;
+	color:black;
+	margin-left:2em;
+	margin-right:2em;
+	border-radius: 25px;
+}
+    </style>
+<div class= "container">
+	     <div class="row">
+            <div class="col-md-4">
+		          <a href="#">
+                  <div class="select_platform_space  " style="font-size:20px;">
+
+                 I want to  publish ads in the form of photos
+                  </div></a>
+            </div>
+            <div class="col-md-4">
+				  <a href="#">
+				  <div class="select_platform_space " style="font-size:20px;">
+                  I want to  publish ads in the form of videos
+                  </div></a>
+			</div>
+               <div class="col-md-4">
+				  <a href="#">
+				  <div class="select_platform_space " style="font-size:20px;">
+                  I want to  publish ads in the form of videos
+                  </div></a>
+			</div>
+	     </div>
+      </div>
+
+
+
+</html>
