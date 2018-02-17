b25617aa2f0b311ae949f4bee1433fc9c306a2f7
NEHA AGARWAL <500061675@DDN.UPES.AC.IN>
2018-02-17 10:29:23 +0530

    changes in crop and sign up

--- Crops/wheat_market_price.php
+++ Crops/wheat_market_price.php
@@ -1,31 +1,34 @@
 <html lang="en">
+
 <head>
-  <title>AD FARM</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Current Market Price</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </head>
+
 <body>
 
-        <?php
+    <?php
         $con = mysqli_connect("localhost","root","","agriculture");
         $query1 = mysqli_query($con," SELECT * FROM wheat_mp ");
 
                                             while($row=mysqli_fetch_array($query1))
                                              {
                                       ?>
-									         <div class="well">
-                                            <?php echo " District name : "." $row[dist_name]<br>";?>
-                                            <?php echo " Mandi name : "." $row[mandi_name]<br>";?>
-                                            <?php echo " Market price : "." $row[mp]<br>";?>
-                                                  </div>
-       <?php
+        <div class="well">
+            <?php echo " District name : "." $row[dist_name]<br>";?>
+            <?php echo " Mandi name : "." $row[mandi_name]<br>";?>
+            <?php echo " Market price : "." $row[mp]<br>";?>
+        </div>
+        <?php
                  }
         ?>
 
-    </body>
+</body>
+
 </html>
