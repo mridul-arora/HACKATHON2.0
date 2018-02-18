ad228066c296e409a232c5f9dc0f0398e8a8d210
NEHA AGARWAL <500061675@DDN.UPES.AC.IN>
2018-02-18 07:46:13 +0530

    hey

--- Transport/search_provider.php
+++ Transport/search_provider.php
@@ -48,11 +48,12 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="AboutUs/index.php">About Us</a></li>
-             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            <li><a href=" ContactUs/index.php ">Contact Us</a></li>
+            <li><a href="../index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                <li><a href="SignUp/index.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="../SignIn/index.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li><a href="../SignOut/index.php"><span class="glyphicon glyphicon-log-out"></span> LogOut</a></li>
+                <li><a href=" ../ContactUs/index.php "><span class="glyphicon glyphicon-earphone"></span>Contact Us</a></li>
+                <li><?php include "../gt.php";?></li>
         </ul>
     </div>
 </nav>
