2057d3189bf3948587b100bbb5ba371b6c87f94b
500060750 <mayuri.kishore123@gmail.com>
2018-02-18 13:03:25 +0530

    contactus

--- ContactUs/index.php
+++ ContactUs/index.php
@@ -20,103 +20,196 @@
     </div>
 </nav>
 </head>
-     <style>
-
- .select_platform_space {
-	border:2px solid white;
-	margin-top:10em;
-	padding:45px;
-   	text-align:center;
-	color:white;
-	margin-left:2em;
-	margin-right:2em;
-	border-radius: 25px;
-          font-weight:1000;
+   <style>
+     bb {
+    width: 320px;
+    padding: 10px;
+    border: 3px solid black;
+    margin: 5;
+    border-radius: 25px;
+    box-shadow: 5px 5px;
+         background-color: sandybrown;
 
 }
-        a:hover{
-            color: white;
-            text-decoration: none;
-        }
-        body{
-            background-image: url("../Assets/images/cu.jpg");
-           background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-        }
+       /*@import url(https://fonts.googleapis.com/css?family=Roboto:400,300,600,400italic);*/
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: antialiased;
+  -o-font-smoothing: antialiased;
+  font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+
+}
+
+body {
+  font-family: sans-serif Helvetica, Arial, sans-serif;
+  font-weight: 1000;
+  font-size: 12px;
+  line-height: 30px;
+  color: black;
+    box-shadow: 5px 5px;
+  background-image: url("../Assets/images/rice.jpg");
+}
+
+.container {
+  max-width: 400px;
+  width: 100%;
+  margin: 10 auto;
+  position: relative;
+}
+
+/*#contact input[type="text"],
+#contact input[type="email"],
+#contact input[type="tel"],
+#contact input[type="url"],
+#contact textarea,
+#contact button[type="submit"]*/{
+  font: 400 12px/16px "Roboto", Helvetica, Arial, sans-serif;
+       }
+
+#contact {
+  background: ;
+  padding: 25px;
+  margin: 150px 0;
+  box-shadow: 20px 20px 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    border-radius: 25px;
+
+}
+
+#contact h3 {
+  display: block;
+  font-size: 30px;
+  font-weight: 300;
+  margin-bottom: 10px;
+}
+
+#contact h4 {
+  margin: 5px 0 15px;
+  display: block;
+  font-size: 13px;
+  font-weight: 400;
+}
+
+fieldset {
+  border: medium none !important;
+  margin: 0 0 10px;
+  min-width: 100%;
+  padding: 0;
+  width: 100%;
+}
+
+#contact input[type="text"],
+#contact input[type="email"],
+#contact input[type="tel"],
+#contact input[type="url"],
+#contact textarea {
+  width: 100%;
+  border: 1px solid #ccc;
+  background: #FFF;
+  margin: 0 0 5px;
+  padding: 10px;
+}
+
+#contact input[type="text"]:hover,
+#contact input[type="email"]:hover,
+#contact input[type="tel"]:hover,
+#contact input[type="url"]:hover,
+#contact textarea:hover {
+  -webkit-transition: border-color 0.3s ease-in-out;
+  -moz-transition: border-color 0.3s ease-in-out;
+  transition: border-color 0.3s ease-in-out;
+  border: 1px solid #aaa;
+}
+
+/*#contact textarea {
+  height: 100px;
+  max-width: 100%;
+  resize: none;
+}*/
+
+/*#contact button[type="submit"] /*{
+  cursor: pointer;
+  width: 100%;
+  border: none;
+  background: #4CAF50;
+  color: #FFF;
+  margin: 0 0 5px;
+  padding: 10px;
+  font-size: 15px;
+}*/
+
+/*#contact button[type="submit"]:hover {
+  background: #43A047;
+  -webkit-transition: background 0.3s ease-in-out;
+  -moz-transition: background 0.3s ease-in-out;
+  transition: background-color 0.3s ease-in-out;
+}*
+
+#contact button[type="submit"]:active {
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.5);
+}*/
+
+.copyright {
+  text-align: center;
+}
+
+/*#contact input:focus,
+#contact textarea:focus {
+  outline: 0;
+  border: 1px solid #aaa;
+}
+
+::-webkit-input-placeholder {
+  color: #888;
+}
+
+:-moz-placeholder {
+  color: #888;
+}
+
+::-moz-placeholder {
+  color: #888;
+}
+
+:-ms-input-placeholder {
+  color: #888;
+}*/
     </style>
     <body>
-<div class= "container">
-    <div class="row">
-        <div class="col-md-12"></div>
-        <CENTER><h1></h1></CENTER>
-    </div>
-	     <div class="row">
-            <div class="col-md-4">
-				  <a href="transport_provider.php">
-				  <div class="select_platform_space " style="font-size:20px;">
-                      <b>MOBILE NO:1234567890</b>
-                  </div></a>
-			</div>
-               <div class="col-md-4">
-		          <a href="transport_user.php">
-                  <div class="select_platform_space " style="font-size:20px;">
+        <div class="container">
+  <form id="contact">
+      <center><h3><BB>CONTACT US</BB></h3></center>
+    <!--<h4>Contact us for custom quote</h4>-->
+      <fieldset><p></p></fieldset>
+       <fieldset><p></p></fieldset>
+       <fieldset><p></p></fieldset>
+    <fieldset>
+        <bb>HELPLINE NUMBER:  9012345678</bb>
+      </fieldset>
+      <fieldset>
+          <p></p>
+      </fieldset>
+    <fieldset>
+      <!--<input placeholder="Your Email Address" type="email" tabindex="2" required>--><bb>EMAIL ID: Amanmishra@gmail.com</bb>
+    </fieldset>
+      <fieldset>
+      <p></p>
+      </fieldset>
 
-                      <b>EMAIL ID: xyz@gmail.com</b>
-                  </div></a>
-            </div>
-             <div class="col-md-4">
-		          <a href="transport_user.php">
-                  <div class="select_platform_space " style="font-size:20px;">
-
-                      <b>ADDRESS: XXYYZZ</b>
-                  </div></a>
-            </div>
-<!--<div class="map-location-area">
-		<div class="map-container">
-			<div class="container">
-						<div class="map-caption">
-							<div class="section-title">
-								<h2>Contact Information</h2>
-							</div>
-							<div class="contact-list">
-								<ul>
-									<li>
-										<a href="#">
-											<i class="icofont icofont-phone"></i>
-											<span>Call : 0123 (1234) 7894</span>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="icofont icofont-envelope"></i>
-											<span>Email : youremail@gmail.com</span>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="icofont icofont-location-pin "></i>
-											<span>House # 200 , Mohakhali , Dhaka</span>
-										</a>
-									</li>
-								</ul>
-							</div>
-							<div class="contact-social-list">
-								<a href="#">
-									<i class="icofont icofont-social-facebook"></i>
-								</a>
-								<a href="#">
-									<i class="icofont icofont-social-twitter"></i>
-								</a>
-								<a href="#">
-									<i class="icofont icofont-envelope"></i>
-								</a>
-								<a href="#">
-									<i class="icofont icofont-social-pinterest"></i>
-								</a>
-							</div>
-						</div>
-	     </div><!--
-      </div>-->
-</body>
-</html>
+    <fieldset>
+      <!--<input placeholder="Your Web Site (optional)" type="url" tabindex="4" required>--><bb>ADDRESS: UPES,BIDOLI, DEHRADUN</bb>
+    </fieldset>
+    <!--<fieldset>
+      <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+    </fieldset>
+      <!--<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+    <p class="copyright">Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a></p>-->
+  </form>
+<
+    </body>
